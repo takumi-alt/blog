6af14e907267d9292de7d6c17839a2e0d4b12abb
takumi-alt <bfca1572@outlook.jp>
2021-12-14 16:02:07 +0900

    head

--- resources/js/components/Head.js
+++ resources/js/components/Head.js
@@ -10,30 +10,30 @@ const Head = () => {
     const [next, setNext] = useState('');
     const [Blog, setBlog] = useState('');
 
-    useEffect(()=> {
+    useEffect(() => {
         setTimeout(() => {
-            for (let i= 0; i < enginier.length; i++) {
-              setTimeout(()=> setNext(next => next + enginier[i]), i * 150);
+            for (let i = 0; i < enginier.length; i++) {
+                setTimeout(() => setNext(next => next + enginier[i]), i * 150);
             }
         }, 1000)
-      }, []);
-    useEffect(()=> {
+    }, []);
+    useEffect(() => {
         setTimeout(() => {
-            for (let i= 0; i < blog.length; i++) {
-              setTimeout(()=> setBlog(Blog => Blog + blog[i]), i * 150);
+            for (let i = 0; i < blog.length; i++) {
+                setTimeout(() => setBlog(Blog => Blog + blog[i]), i * 150);
             }
         }, 3000)
-      }, []);
+    }, []);
 
-    
+
 
     return (
         <>
             <div className="flex justify-center items-center h-32 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-center">
-                    <h3 className="font-black text-6xl text-regal-white m-4">{next}</h3>
-                    <h3 className="font-black text-6xl text-regal-white m-4">{Blog}!</h3>
+                <h3 className="font-black text-6xl text-regal-white m-4">{next}</h3>
+                <h3 className="font-black text-6xl text-regal-white m-4">{Blog}</h3>
             </div>
-           
+
         </>
     )
 }
