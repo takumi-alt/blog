d81ed9ac22a3b99eb9f5e8db93d37595746f98a3
takumi-alt <bfca1572@outlook.jp>
2021-12-18 12:07:53 +0900

    mix.jsの編集
    tailwindだけに変更したつもりが、変わらなかった。

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .react()
-    .postCss('resources/css/app.css', 'public/css', [
+    .postCss('resources/css/app.css', [
         require('tailwindcss'),
     ])
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss',);
+
+    // 'public/css',
     
