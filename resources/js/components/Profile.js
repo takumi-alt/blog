c4e7fda61817da0d86298be917125d94feafc018
takumi-alt <bfca1572@outlook.jp>
2021-12-28 16:33:01 +0900

    profile.jsのflex対応を追加した

--- resources/js/components/Profile.js
+++ resources/js/components/Profile.js
@@ -6,49 +6,62 @@ import {FaTwitter} from 'react-icons/fa';
 const Profile = () => {
     return (
         <div className="bg-gray-200">
-            <div className="w-3/4 sm:w-2/3 mx-auto pt-12">
-                {/* name,img */}
-                <div className="lg:w-3/4 mx-auto">
-                <div className="mb-8">
-                    <img src="/storage/profile.jpg" className="mb-4 w-32 h-32 mx-auto rounded-full"></img>
-                    <p className="text-center font-bold text-xl text-green-500 mb-2">TAKUMI MIMA</p>
-                    <div className="w-32 mx-auto flex justify-center">
-                        <a className="w-16 text-purple-400 text-3xl mx-auto"><FaTwitter className="mx-auto"/></a>
-                        <a className="w-16 text-purple-400 text-3xl mx-auto"><FaInstagram className="mx-auto"/></a>
-                    </div>
-                </div>
-                <div className="mx-auto mb-12">
-                    <p className="text-xl font-medium w-32 bg-purple-500 text-center rounded-lg shadow-purple2">About</p>
-                    <p className="border-t-2 border-b-2 border-green-500 py-4 md:text-lg text-regal-black">
-                        ご訪問ありがとうございます。サイト運営者の美馬 巧と申します。<br/>
-                        大阪府の私立大学に通う大学三年生です。将来はエンジニアとして活躍したいと思い、独学でプログラミングを勉強中です。<br/>
-                        お問い合わせは、Contactまたはinstagram、twitterからお願いします。
-
-                    </p>
-                </div>
-                {/* language */}
-                <div className="mx-auto mb-12">
-                    <p className="text-xl font-medium w-32 bg-purple-500 text-center rounded-lg shadow-purple2">Language</p>
-                    <p className="md:text-lg">
-                        HTML<br/>
-                        CSS<br/>
-                        PHP<br/>
-                        Python<br/>
-                    </p>
-                </div>
-                {/* Work */}
+            <div className="w-3/4 lg:w-5/6 mx-auto pt-12">
                 <div className="mx-auto">
-                    <p className="text-xl font-medium w-32 bg-purple-500 text-center rounded-lg shadow-purple2">My Work</p>
-                    <div className="border-t-2 border-b-2 border-green-500 py-4">
-                        <img src="/storage/blog2.png" className="shadow-xl mb-4"></img>
-                        <p className="text-lg md:text-xl text-purple-500 bg-green-500 text-center w-48 shadow-green">No.1 engineer blog</p>
-                        <p className="md:text-lg">
-                            こちらのブログは私が初めて本格的に作製したサイトになります。<br/>
-                            CRUD機能を搭載しており、SPA化もしています。<br/>
-                            使用技術としては主に、Laravel, React, Tailwindcssを用いています。
-                        </p>
+
+                    <div className="w-full">
+                        {/* name,img,icon*/}
+                        <section className="mb-8">
+                            <img src="/storage/profile.jpg" className="mb-4 w-32 h-32 mx-auto rounded-full"></img>
+                            <p className="text-center font-bold text-xl text-green-500 mb-2">TAKUMI MIMA</p>
+                            <div className="w-32 mx-auto flex justify-center">
+                                <a className="w-16 text-purple-400 text-3xl mx-auto"><FaTwitter className="mx-auto"/></a>
+                                <a className="w-16 text-purple-400 text-3xl mx-auto"><FaInstagram className="mx-auto"/></a>
+                            </div>
+                        </section>
+                        
                     </div>
-                </div>
+
+                    <div className="mx-auto md:w-4/5 md:flex justify-between">
+                        {/* About */}
+                        <section className="mx-auto mb-12 md:w-2/3">
+                            <p className="text-xl font-medium w-32 bg-purple-500 text-center rounded-lg shadow-purple2">About</p>
+                            <p className="border-t-2 border-green-500 py-4 lg:text-lg text-regal-black">
+                                ご訪問ありがとうございます。サイト運営者の美馬 巧と申します。<br/>
+                                大阪府の私立大学に通う大学三年生です。将来はエンジニアとして活躍したいと思い、独学でプログラミングを勉強中です。<br/>
+                                お問い合わせは、Contactまたはinstagram、twitterからお願いします。
+                            </p>
+                        </section>
+
+                        {/* language */}
+                        <section className="mx-auto mb-12 md:w-1/3 md:pl-20">
+                            <p className="text-xl font-medium w-32 bg-purple-500 text-center rounded-lg shadow-purple2">Language</p>
+                            <p className="md:text-lg border-t-2 border-green-500">
+                                ◆HTML<br/>
+                                ◆CSS<br/>
+                                ◆PHP<br/>
+                                ◆Python<br/>
+                            </p>
+                        </section>
+                    </div>
+                    {/* Work */}
+                    <section className="md:w-4/5 mx-auto">
+                        <p className="text-xl font-medium w-32 bg-purple-500 text-center rounded-lg shadow-purple2">My Work</p>
+                        <div className="border-t-2 border-b-2 border-green-500 py-4 md:flex justify-between">
+                            <div className="md:w-2/5">
+                                <img src="/storage/blog2.png" className="shadow-xl mb-4"></img>
+                                <p className="text-lg md:text-xl text-purple-500 bg-green-500 text-center w-48 shadow-green">No.1 engineer blog</p>
+                                <p className="">
+                                    こちらのブログは私が初めて本格的に作製したサイトになります。<br/>
+                                    CRUD機能を搭載しており、SPA化もしています。<br/>
+                                    使用技術としては主に、Laravel, React, Tailwindcssを用いています。
+                                </p>
+                            </div>
+                            <div className="md:w-2/5">
+
+                            </div>
+                        </div>
+                    </section>
 
                 </div>
 
