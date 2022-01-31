0b6f0bfc7053dae1e544d8ea40ef32887f6dce60
takumi-alt <bfca1572@outlook.jp>
2022-01-31 12:17:59 +0900

    hotterの変更

--- resources/js/components/Hooter.js
+++ resources/js/components/Hooter.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Hoot = () => {
     return (
-        <div className="bg-regal-white h-20">
-            <span className="font-black text-regal-black ml-12 text-2xl">
-                takumi mima
-            </span>
+        <div className="bg-regal-white text-center py-4">
+            <div className="font-black text-regal-black text-2xl">
+                engineer blog
+            </div>
+            <div className="text-xs mb-4">engineeeer.site</div>
+
+            <section className="flex justify-between mx-auto w-4/5 text-xs sm:text-sm">
+                <div>プロフィール</div>
+                <div>お問い合わせ</div>
+                <div>プライバシーポリシー</div>
+            </section>
         </div>
     )
 }
